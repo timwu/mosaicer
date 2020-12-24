12f72c432946c346bfc617f10bc64604e8c32415
Tim Wu <tim.wu.0@gmail.com>
2020-12-24 11:55:42 -0800

    Make analyzer configurable

--- analysis/analyzers.go
+++ analysis/analyzers.go
@@ -7,14 +7,24 @@ import (
 	"github.com/timwu/mosaicer/util"
 )
 
+const (
+	samples = 2
+)
+
 // Simple performs a simple analysis of the given image into a 1x1 and aspect-ratio sized image samples
 func Simple(img image.Image) (*ImageData, error) {
 	data := &ImageData{
 		AspectRatio: util.AspectRatio(img),
-		Samples:     make([]*image.NRGBA, 0),
+		Samples:     make([]*image.NRGBA, samples),
 	}
 
-	data.Samples = append(data.Samples, imaging.Resize(img, 1, 1, imaging.NearestNeighbor))
-	data.Samples = append(data.Samples, imaging.Resize(img, data.AspectRatio.X, data.AspectRatio.Y, imaging.NearestNeighbor))
+	for i := 0; i < samples; i++ {
+		size := data.AspectRatio.Mul(i)
+		if i == 0 {
+			size = image.Point{X: 1, Y: 1}
+		}
+		data.Samples[i] = imaging.Resize(img, size.X, size.Y, imaging.NearestNeighbor)
+	}
+
 	return data, nil
 }
