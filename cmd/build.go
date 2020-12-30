ed91eff12afc39a9ee823e0957051a264d87deba
Tim Wu <timothywu@google.com>
2020-12-30 11:48:20 -0800

    Add licenses

--- cmd/build.go
+++ cmd/build.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cmd
 
 import (
@@ -28,8 +42,8 @@ var (
 	fuzziness              = 0
 	referencePatchMultiple = 1
 	cpuprofile             = ""
-	tileSelectionThreads   = 2
-	tilingThreads          = 1
+	tileSelectionThreads   = 10
+	tilingThreads          = 16
 )
 
 func init() {
